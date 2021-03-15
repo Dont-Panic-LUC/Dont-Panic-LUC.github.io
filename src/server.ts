05afc54418f88dd2e91da063b3ee94af16fbddc3
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-03-15 17:42:19 -0500

    Remove subdirectory support

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
   .use(
-    'repo-name', // For GitHub pages
+    // 'repo-name', // For GitHub pages
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
