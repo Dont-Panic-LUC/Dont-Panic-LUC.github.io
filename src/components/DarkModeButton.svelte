a3f9a4f1c1266576e7681566b6ca8d512ed4d3b5
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-03-16 02:29:47 -0500

    Add ~dark theme~

---
+++ src/components/DarkModeButton.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+  import IoIosSunny from 'svelte-icons/io/IoIosSunny.svelte';
+  import IoIosMoon from 'svelte-icons/io/IoIosMoon.svelte';
+
+  type ColorMode = 'light' | 'dark';
+  let theme: ColorMode = 'dark';
+
+  const getPrefersDarkMode = () =>
+    window ? window.matchMedia('(prefers-color-scheme: dark)').matches : true;
+
+  const getStoredTheme = () =>
+    localStorage.getItem('theme') as ColorMode | undefined;
+
+  const setLightTheme = () => {
+    document.body.classList.toggle('theme--light', true);
+    document.body.classList.toggle('theme--dark', false);
+    theme = 'light';
+  };
+
+  const setDarkTheme = () => {
+    document.body.classList.toggle('theme--light', false);
+    document.body.classList.toggle('theme--dark', true);
+    theme = 'dark';
+  };
+
+  // ts-lint was freaking out, so this "_" argument is just to make that go away
+  const checkTheme = (_) => {
+    const storedTheme = getStoredTheme();
+
+    if (storedTheme === 'light') setLightTheme();
+    else if (storedTheme === 'dark') setDarkTheme();
+    else if (document.body.classList.contains('theme--light')) setLightTheme();
+    else if (document.body.classList.contains('theme--light')) setDarkTheme();
+    else if (getPrefersDarkMode()) setDarkTheme();
+    else setLightTheme();
+  };
+
+  const toggleColorMode = () => {
+    if (theme === 'light') {
+      setDarkTheme();
+    } else if (theme === 'dark') {
+      setLightTheme();
+    }
+
+    theme = document.body.classList.contains('theme--light') ? 'light' : 'dark';
+    localStorage.setItem('theme', theme);
+  };
+</script>
+
+<button
+  on:click={toggleColorMode}
+  use:checkTheme
+  aria-label="toggle color mode"
+>
+  <div in:fade>
+    {#if theme === 'light'}
+      <IoIosMoon />
+    {:else}
+      <IoIosSunny />
+    {/if}
+  </div>
+</button>
+
+<style>
+  button {
+    text-decoration: none;
+    transition: 0.1s;
+    padding: 0.75em 1.5em;
+    border-radius: 4px;
+    font-weight: 600;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+  }
+  button:hover {
+    background: var(--background-hover);
+  }
+  button > div {
+    height: 1.25rem;
+    width: 1.25rem;
+    color: var(--color);
+  }
+
+  @media screen and (max-width: 600px) {
+    button > div {
+      height: 1.15rem;
+      width: 1.15rem;
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,33 @@
 <script>
-	import successkid from 'images/successkid.jpg';
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Hey, this is the Don't Panic! Site</h1>
+<p>That's pretty neat</p>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<style>
+  h1,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
