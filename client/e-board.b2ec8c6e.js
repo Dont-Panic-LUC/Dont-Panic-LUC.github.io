33bf28ea4f7fae6ff26c4678d81b3a61ac483159
acrose99 <acrbrazil99@gmail.com>
2021-03-19 02:39:59 +0000

    Deploying to gh-pages from  @ 7f63bae5c747972441615896a765490552c1d7c9 🚀

--- client/e-board.b2ec8c6e.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e,q as a,d as n,n as o}from"./client.f97c7884.js";function c(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-n9twzn"]',document.head).forEach(n),this.h()},h(){document.title="E-Board"},m:o,p:o,i:o,o:o,d:o}}export default class extends t{constructor(t){super(),s(this,t,null,c,e,{})}}
