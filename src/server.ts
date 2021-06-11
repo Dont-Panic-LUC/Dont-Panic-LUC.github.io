abb2592d0e4246092c4927d90869a3daa28ff56d
acrose99 <acrosemail@gmail.com>
2021-06-11 18:03:47 -0500

    Fix: Bandaid solution to an issue Sapper currently has with a new version of rollup, for typescript projects.

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,12 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-  .use(
-    // 'Dont-Panic-LUC.github.io', // For GitHub pages
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log('error', err);
-  });
+    .use(
+        // compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    // .listen(PORT, err => {
+    // 	if (err) console.log('error', err);
+    // });
+    .listen(PORT);
