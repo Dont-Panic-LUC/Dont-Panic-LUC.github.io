29d4ea4ac9c9aff185c23fabf1b5d6ceac42beb2
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-03-13 23:23:34 -0500

    Update E-Board

--- src/routes/e-board.svelte
+++ src/routes/e-board.svelte
@@ -5,8 +5,61 @@
 <svelte:head>
   <title>E-Board</title>
 </svelte:head>
-<h3>* Subject to change, as The E-Board is in process of transitioning to Fall 2021 * </h3>
+<h1>E-Board</h1>
+<!-- <h3>* Subject to change, as The E-Board is in process of transitioning to Fall 2021 * </h3> -->
 <section>
+  
+  <Teammate
+    src="assets/e-board/Tamanna-2.jpeg"
+    role="President"
+    name="Tamanna Miah"
+    position="50% 65%"
+    link=""
+    linkTitle="→"
+  />
+  
+  <Teammate
+    src="assets/e-board/Aaron-2.jpeg"
+    role="Vice President"
+    name="Aaron Juco"
+    link=""
+    linkTitle="→"
+  />
+
+  <Teammate
+    src="assets/e-board/Laila.jpeg"
+    role="Head of Communications"
+    name="Laila Hamdan"
+    link=""
+    linkTitle="→"
+  />
+
+  <Teammate
+    src="assets/e-board/Tristian.jpeg"
+    role="Treasurer"
+    name="Tristian"
+    position="50% 60%"
+    link=""
+    linkTitle="→"
+  />
+  
+  <Teammate
+    src="assets/e-board/Jenna.jpeg"
+    role="CAN Representative"
+    name="Jenna Hazlip"
+    link=""
+    linkTitle="→"
+  />
+
+  <Teammate
+    src="assets/e-board/Isaiah.JPG"
+    role="Lead Website Developer"
+    name="Isaiah Bouromphongsa"
+    position="50% 60%"
+    link=""
+    linkTitle=""
+  />
+  
   <Teammate
     src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fvignette1.wikia.nocookie.net%2Fcriminal-case-grimsborough%2Fimages%2F2%2F26%2FUnknown_Character.png%2Frevision%2Flatest%3Fcb%3D20141106142941&f=1&nofb=1"
     role="Head of Applying"
@@ -14,8 +67,8 @@
     link="https://docs.google.com/forms/d/e/1FAIpQLSdEV5soG_fWijQtK8RBF5-23EPHdIfYOqzjZLnDAc-PQVnkoA/viewform"
     linkTitle="Click here to apply →"
   />
-  
-  <Teammate
+
+  <!-- <Teammate
     src="assets/e-board/Julia.jpg"
     role="President"
     name="Julia Bennett"
@@ -64,7 +117,7 @@
     position="50% 60%"
     link="https://viciiiz.github.io"
     linkTitle="Portfolio →"
-  />
+  /> -->
 
   <!-- <Teammate
     src={'assets/e-board/tien.jpg'}
@@ -146,9 +199,13 @@
 </section>
 
 <style>
-  h3 {
+  /* h3 {
     color: var(--text);
     text-align: center;
+  } */
+  h1 {
+    text-align: center;
+    color: var(--primary);
   }
   section {
     padding: 2em;
