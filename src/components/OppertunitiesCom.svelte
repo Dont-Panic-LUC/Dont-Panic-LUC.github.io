2d35d555cbaf7ea9c8a7e8174ef225b72c558be6
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-09-02 23:38:04 -0500

    Test oppertunities tab

---
+++ src/components/OppertunitiesCom.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import dayjs from 'dayjs';
+  
+    export let oppertunities: {
+      title: string;
+      image: string;
+      date: string;
+      html: any;
+    }[];
+  
+    export let title: string = 'Upcoming Oppertunites';
+    export let isPast: boolean = false;
+  
+    const today = new Date().toISOString();
+  
+    const sortedEvents = oppertunities
+      .filter((e) => (isPast ? e.date < today : e.date >= today))
+      .sort((a, b) => {
+        if (a.date < b.date) return 1;
+        else return -1;
+      });
+  </script>
+
+<div id="container">
+    <h1>{title}</h1>
+    <div class="card-container">
+      {#each sortedEvents as e}
+        <article class="card">
+          {#if e.image}
+            <div class="card-image-container">
+              <img
+                src={`assets/content/events/${e.image}`}
+                alt={`Banner for the event ${e.title}`}
+              />
+            </div>
+          {/if}
+          <div class="card-content">
+            <div class="card-top">
+              {#if e.date === 'TBD' || e.date === 'Upcoming'}
+                <p>Exact Time Forthcoming</p>
+                {:else if e.date}
+                <p>{dayjs(e.date).format('MMM DD, hh:mma')}</p>
+              {/if}
+              <h2>{e.title}</h2>
+            </div>
+            {@html e.html}
+          </div>
+        </article>
+      {/each}
+    </div>
+  </div>
+  
+  <style>
+    .card {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      background: var(--bg-item);
+      color: var(--text);
+  
+      border-radius: 4px;
+      margin-bottom: 1em;
+  
+      box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.18);
+    }
+  
+    .card:last-of-type {
+      margin-bottom: 0em;
+    }
+  
+    @media screen and (max-width: 45em) {
+      .card {
+        flex-direction: column;
+      }
+    }
+  
+    .card-image-container {
+      height: 20em;
+      padding: 2em 0em 2em 2em;
+    }
+  
+    @media screen and (max-width: 60em) {
+      .card-image-container {
+        height: 16em;
+        padding: 2em 0em 2em 2em;
+      }
+    }
+  
+    @media screen and (max-width: 45em) {
+      .card-image-container {
+        height: 12em;
+        padding: 2em 2em 0em 2em;
+      }
+    }
+  
+    .card-image-container img {
+      height: 100%;
+    }
+  
+    .card-content {
+      padding: 2em;
+    }
+  
+    .card-top h2 {
+      margin: 0;
+    }
+  
+    h1 {
+      font-size: 1.5em;
+    }
+  
+    @media (min-width: 45em) {
+      h1 {
+        font-size: 1.75em;
+      }
+    }
+    @media (min-width: 60em) {
+      h1 {
+        font-size: 2em;
+      }
+    }
+  </style>
---
+++ src/routes/oppertunities.svelte
@@ -0,0 +1,42 @@
+<script context="module" lang="ts">
+    export function preload() {
+      return this.fetch(`events.json`)
+        .then((r: { json: () => any }) => r.json())
+        .then((events: { slug: string; title: string; html: any }[]) => {
+          return { events };
+        });
+    }
+  </script>
+  
+  <script lang="ts">
+    import OppertunitiesCom from '../components/OppertunitiesCom.svelte';
+  
+    export let oppertunities: {
+      title: string;
+      image: string;
+      date: string;
+      html: any;
+    }[];
+  </script>
+
+<svelte:head>
+    <title>Oppertunities</title>
+  </svelte:head>
+  
+  <div id="container">
+    <OppertunitiesCom {oppertunities} />
+    <div id="spacer" />
+    <OppertunitiesCom {oppertunities} isPast title="Past Events" />
+  </div>
+  
+  <style>
+    #container {
+      padding: 2em;
+    }
+  
+    #spacer {
+      margin-top: 4em;
+    }
+  </style>
+  
+
