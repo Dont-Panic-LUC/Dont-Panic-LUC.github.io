6b74908ae02810b36d719baa7770c4bd9580fa82
IsaiahBouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-09-03 05:06:59 +0000

    Deploying to gh-pages from @ Dont-Panic-LUC/Dont-Panic-LUC.github.io@c8ce42b00f12c8c03355be215e4a03dc433964e0 🚀

--- client/alumni.4f4ac451.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as a}from"./client.c2a78f89.js";class l extends s{constructor(s){super(),t(this,s,null,null,a,{})}}export{l as default};
---
+++ client/alumni.a3cc826b.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e}from"./client.520e9209.js";class l extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{l as default};
