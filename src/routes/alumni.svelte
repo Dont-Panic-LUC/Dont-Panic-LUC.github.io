225b2eb20f44868ec481b75f015b17a813411f2b
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-09-03 01:24:49 -0500

    Uhhtempt

--- src/routes/alumni.svelte
+++ src/routes/alumni.svelte
@@ -1,1 +1,5 @@
+<script>
+	let name = 'world';
+</script>
+
 <h1>Alumni page under construction!</h1>
--- src/routes/professors.svelte
+++ src/routes/professors.svelte
@@ -1,1 +1,5 @@
+<script>
+	let name = 'world';
+</script>
+
 <h1>Professors page under construction</h1>
